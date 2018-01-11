88e2a12975e1ac3026e17175b11ada01fcd44131
Wizardgreen <t0937383@gmail.com>
2018-01-11 10:27:08 +0800

    build navigation and part of about section

--- js/initFullPage.js
+++ js/initFullPage.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
-    sectionsColor: ['#fff', '#fff', '#fff', '#fff', '#fff', '#fff', '#222'],
+    sectionsColor: ['rgba(0,0,0,.3)', '#fff', '#fff', '#fff', '#fff', '#fff', '#222'],
     scrollOverflow: true,
     menu: '#header'
+  });
+  $(document).on('click', '#btnGoDown', function(){
+    $.fn.fullpage.moveSectionDown();
   });
 });
