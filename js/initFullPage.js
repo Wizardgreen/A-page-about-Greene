f544b6f2ab54c48162c909556c146339ea948985
Wizardgreen <t0937383@gmail.com>
2018-01-14 23:40:17 +0800

    part of exp section

--- js/initFullPage.js
+++ js/initFullPage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
-    sectionsColor: ['rgba(0,0,0,.3)', '#fff', '#fff', '#fff', '#fff', '#fff', '#222'],
+    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', '#222'],
     scrollOverflow: true,
     menu: '#header'
   });
