aba65009e396d7c89806700e45290d3e9ad04844
Wizardgreen <t0937383@gmail.com>
2018-01-16 18:06:29 +0800

    complete works section

--- js/initFullPage.js
+++ js/initFullPage.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
-    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', '#222'],
+    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.5)', 'rgba(0,0,0,.05)', '#222'],
     scrollOverflow: true,
     menu: '#header'
   });
