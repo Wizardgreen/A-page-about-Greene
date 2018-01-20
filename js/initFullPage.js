787c3e0c9a2dc66c1db1eee3859e0ea5f40a384d
Wizardgreen <t0937383@gmail.com>
2018-01-20 23:19:37 +0800

    complete exp section

--- js/initFullPage.js
+++ js/initFullPage.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
     anchors:['home', 'about', 'exp', 'skill', 'works', 'contact', 'footer'],
-    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.5)', 'rgba(0,0,0,.05)', '#222'],
+    sectionsColor: ['rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', 'rgba(0,0,0,.05)', '#222'],
     scrollOverflow: true,
     menu: '#header'
   });
   $(document).on('click', '#btnGoDown', function(){
     $.fn.fullpage.moveSectionDown();
   });
+  $(".expCard").hide();
+  
+  let bubble = [".babyBlue", ".hotPink", ".yellow", ".orange", ".green", ".purple"];
+  let card = [".translate", ".pianoClub", ".shopping", ".government", ".campaign", ".technical"];
+  for(let i=0; i<6; i++) {
+    $(bubble[i]).hover(function() {
+      $(card[i]).fadeIn(60);
+    }, function() {
+      $(card[i]).fadeOut(60);
+    });
+  }
 });
